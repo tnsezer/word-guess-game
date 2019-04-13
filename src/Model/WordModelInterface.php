93c1afd24c9da6887912d0ba846376b373b6cc5d
Tan <tnsezer@gmail.com>
2019-04-14 00:29:38 +0200

    added interface

---
+++ src/Model/WordModelInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tan
+ * Date: 14.04.2019
+ * Time: 00:28
+ */
+
+namespace App\Model;
+
+
+interface WordModelInterface
+{
+
+}
---
+++ src/Repository/GameRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tan
+ * Date: 14.04.2019
+ * Time: 00:26
+ */
+
+namespace App\Repository;
+
+
+interface GameRepositoryInterface
+{
+    public function getChance(): int;
+    public function guess(string $word): bool;
+    public function getStatus(): \stdClass;
+    public function lastResult(): string;
+    public function getWord(): string;
+}
